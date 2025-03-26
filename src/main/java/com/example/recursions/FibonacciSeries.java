503ce0d18e2768e74df477900f3f697daeedf364
AryanRajputx1 <aryan.rajput5@icloud.com>
2025-03-25 21:21:23 -0400

    FibonacciSeries Using recursion

---
+++ src/main/java/com/example/recursions/FibonacciSeries.java
@@ -0,0 +1,14 @@
+package com.example.recursions;
+
+public class FibonacciSeries {
+    public static void main(String[] args) {
+System.out.println(fibo(6));
+    }
+    static int fibo(int n){
+        // base condtion
+        if (n<2 ){
+            return n;
+        }
+        return  fibo(n-1)+ fibo(n-2);
+    }
+}
